e12e163e5ac436d5426494a4ed20401d5304d11b
Daniël W. Crompton <daniel.crompton@gmail.com>
2015-01-06 14:18:13 +0100

    Added missing parentheses, error with python < 3

--- simple_ctrl_fuzzer.py
+++ simple_ctrl_fuzzer.py
@@ -13,18 +13,18 @@ def is_alive(device):
     except usb.core.USBError as e:
 
         if e.backend_error_code == -4: # LIBUSB_ERROR_NO_DEVICE
-            print "\nDevice not found!"
+            print ( "\nDevice not found!" )
             sys.exit()
 
         if e.backend_error_code == -3: # LIBUSB_ERROR_ACCESS
-            print "\nAccess denied to device!"
+            print ( "\nAccess denied to device!" )
             sys.exit()
 
-        print "\nGET_STATUS returned error %i" % e.backend_error_code
+        print ( "\nGET_STATUS returned error %i" % e.backend_error_code )
         return False
 
     if len(res) != 2:
-        print "\nGET_STATUS returned %u bytes: %s" % (len(res), binascii.hexlify(res))
+        print ( "\nGET_STATUS returned %u bytes: %s" % (len(res), binascii.hexlify(res)) )
         return False
 
     return True
@@ -37,17 +37,17 @@ def is_alive(device):
         elif e.backend_error_code == -6: #LIBUSB_ERROR_BUSY
             raise Exception('Function check failed: function is busy!')
         else:
-            print "Function check failed: usb error %i" % e.backend_error_code
+            print ( "Function check failed: usb error %i" % e.backend_error_code )
             return False
 
     try:
         res = device.read(0x82, 0x400)
     except usb.core.USBError as e:
-        print "Function check failed: usb error %i" % e.backend_error_code
+        print ( "Function check failed: usb error %i" % e.backend_error_code )
         return False
 
     if len(res) != 0x20d:
-        print "Function check returned %x bytes: %s" % (len(res), binascii.hexlify(res))
+        print ( "Function check returned %x bytes: %s" % (len(res), binascii.hexlify(res)) )
         return False
         
     return True
@@ -60,17 +60,17 @@ def TestCtrlTransfer(device, rt, r, v, i):
 
         try:
             res = device.ctrl_transfer(rt&0x80, r, v, i, bytearray().fromhex(u'ff'*size), timeout=250)
-            print 'OUT %0.2x %0.2x %0.4x %0.4x res(%u) len(%u)' % (rt, r, v, i, res, size)
+            print ( 'OUT %0.2x %0.2x %0.4x %0.4x res(%u) len(%u)' % (rt, r, v, i, res, size) )
         except usb.core.USBError as e:
             if (e.backend_error_code != -9 and e.backend_error_code != -1): # ignore LIBUSB_ERROR_PIPE and LIBUSB_ERROR_IO
-                print 'OUT %0.2x %0.2x %0.4x %0.4x err(%i) len(%u)' % (rt, r, v, i, e.backend_error_code, size)
+                print ( 'OUT %0.2x %0.2x %0.4x %0.4x err(%i) len(%u)' % (rt, r, v, i, e.backend_error_code, size) )
 
         try:
             res = device.ctrl_transfer(rt|0x80, r, v, i, size, timeout=250)
-            print 'IN  %0.2x %0.2x %0.4x %0.4x data(%u) len(%u):\t%s' % (rt, r, v, i, len(res), size, binascii.hexlify(res))
+            print ( 'IN  %0.2x %0.2x %0.4x %0.4x data(%u) len(%u):\t%s' % (rt, r, v, i, len(res), size, binascii.hexlify(res)) )
         except usb.core.USBError as e:
             if (e.backend_error_code != -9 and e.backend_error_code != -1): # ignore LIBUSB_ERROR_PIPE and LIBUSB_ERROR_IO
-                print 'IN  %0.2x %0.2x %0.4x %0.4x err(%i) len(%u)' % (rt, r, v, i, e.backend_error_code, size)
+                print ( 'IN  %0.2x %0.2x %0.4x %0.4x err(%i) len(%u)' % (rt, r, v, i, e.backend_error_code, size) )
 
         if i%10==0:
             if not is_alive(device):
